1aa70c3dd76aa5d458965ea0f08cbd74e40079c8
Josias Iquabius <iquabius@gmail.com>
2019-01-25 15:32:38 -0300

    Set up coolPromptTheme for project switching

--- xmonad.hs
+++ xmonad.hs
@@ -18,8 +18,8 @@ myKeys =
   , ("M-<Return>"       , spawn myTerminal)
 
   -- Workspaces & Projects
-  , ("M-w"              , switchProjectPrompt warmPromptTheme)
-  , ("M-S-w"            , shiftToProjectPrompt warmPromptTheme)
+  , ("M-w"              , switchProjectPrompt coolPromptTheme)
+  , ("M-S-w"            , shiftToProjectPrompt coolPromptTheme)
   ]
 
 -- Define custom Pretty Printing option
@@ -85,7 +85,7 @@ base03  = "#002b36"
 -- base0   = "#839496"
 -- base1   = "#93a1a1"
 -- base2   = "#eee8d5"
--- base3   = "#fdf6e3"
+base3   = "#fdf6e3"
 yellow  = "#b58900"
 -- orange  = "#cb4b16"
 -- red     = "#dc322f"
@@ -94,6 +94,7 @@ magenta = "#d33682"
 blue    = "#268bd2"
 -- cyan    = "#2aa198"
 -- green   = "#859900"
+solarGreen = "#7c7526"          -- variations: #7c7526 #2d7a01 #7c7526
 
 -- sizes
 prompt      = 20
@@ -118,11 +119,17 @@ myPromptTheme = def
     { font                  = myFont
     , bgColor               = base03
     , fgColor               = active
-    , fgHLight              = base03
+    , fgHLight              = base3
     , bgHLight              = active
     , borderColor           = base03
     , promptBorderWidth     = 0
     , height                = prompt
+    , position              = Top
+    }
+
+coolPromptTheme = myPromptTheme
+    { bgColor               = solarGreen
+    , fgColor               = base3
     , position              = Top
     }
 
